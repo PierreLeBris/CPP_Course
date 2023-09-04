8068f678c2ae68bafe60914fd3a930fe5127dc54
Pierre LE BRIS <50422307+PierreLeBris@users.noreply.github.com>
2023-09-04 10:22:30 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,14 @@
 
 using namespace std;
 
+Personnage::Personnage()
+{
+    m_vie = 100;
+    m_mana = 100;
+    m_nomArme = "Epee rouillee";
+    m_degatsArme = 10;
+}
+
 int main()
 {
     Personnage david, goliath;
